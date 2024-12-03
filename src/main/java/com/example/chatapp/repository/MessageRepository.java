da6b9f828dc10e4ed6a80be38c5ae904b66995f6
torzchi <117668600+torzchi@users.noreply.github.com>
2024-12-03 10:23:36 +0200

    save messages in db

---
+++ src/main/java/com/example/chatapp/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.example.chatapp.repository;
+
+import com.example.chatapp.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByType(Message.MessageType type);
+}
